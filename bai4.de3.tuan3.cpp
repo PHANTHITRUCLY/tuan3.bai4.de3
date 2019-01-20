211dbc1b81eef3eabeade19c3e6f5f6b77cec7a7
Phan Thị Trúc Ly <46109438+PHANTHITRUCLY@users.noreply.github.com>
2019-01-20 19:31:06 +0700

    update.bai4.de3.tuan3

--- bai4.de3.tuan3.cpp
+++ bai4.de3.tuan3.cpp
@@ -4,17 +4,20 @@
 #include <iostream>
 using namespace std;
 #include <fstream>
-int n=5; // tong so xe o to
-int cv[5]; // xe dung hen
-int x[5]={1,2,3,4,5};// so thu tu cua xe
-int y[5]={6,3,5,7,2};//thoi gian sua chua xe
-int z[5]={8,4,15,20,3};//thoi diem ban giao xe
-int hieusongay[5]; // luu hieu so ngay cua z-y;
-int songaythua[5]; // so ngay thua ra sau moi lan sua chua xe
+#define so 9 // so luong
+
+int n; // tong so xe o to
+int cv[so]; // xe dung hen
+int x[so];//={1,2,3,4,5};// so thu tu cua xe (ten xe)
+int y[so];//={6,3,5,7,2};//thoi gian sua chua xe
+int z[so];//={8,4,15,20,3};//thoi diem ban giao xe
+int hieusongay[so]; // luu hieu so ngay cua z-y;
+int songaythua[so]; // so ngay thua ra sau moi lan sua chua xe
 int tong1;
 int tong2;
-
-
+int ktratre[so]; // ktra xe nao tre hen de in ra xe tre han
+int demxe=0;
+int t;
 void Ghifile(FILE *f,const char *duongdan1){ //ghi cac ptu vao file
 
 	f= fopen(duongdan1,"wt");
@@ -66,11 +69,11 @@ void Docfile(const char *duongdan1,const char *duongdan2){
 		
 		printf("\n\nTong sl xe o to la: ");
 		doc>>n; // doc tu fghi
-		ghi<<n<<endl; // in ra fxuat 
+		ghi<<n; // in ra fxuat 
 		printf(" %d\n",n); // in ra man hinh
 		ghi<<endl;
 		
-		printf("\n O to: ");
+		printf("\n O to:\t\t\t ");
 		for(int i=0; i< n; i++){
 			
 			doc>>x[i];
@@ -113,33 +116,86 @@ void Xuat(){
 	
 		}
 }
-void Xuatkq(){ // tiep tuc ghi noi dung vao file kq
+
+void Xuatfilekq(){ // tiep tuc ghi noi dung vao file kq
 		fstream ghi;
 		ghi.open("fxuat.txt", ios::out | ios::app); // ios::app khai bao de tiep tuc ghi vao file o tren.
-		printf("\nTen xe dung hen: ");
-		ghi<<"\nTen xe dung hen: "<<endl;
-		for(int i=0; i<n;i++){
-			printf("%d\t",cv[i]);
-			ghi<<cv[i]<<" ";	
-		}
-			//-----------------------------Phan duoi chi xuat ra man hinh
-		printf("\nHieu so ngay xe oto: ");			
-		for(int i=0; i<n;i++)
-			printf("%d\t",hieusongay[i]);
 		
-		printf("\nSo ngay sua con du sau moi lan sua xe oto: ");			
-		for(int i=0; i<n;i++)
-			printf("%d\t",songaythua[i]);
-			
-		printf("\nTong so ngay thuc hien: ");	
-			printf("%d\t",tong2);
-			
-		printf("\nTong so xe dung hen: ");
-			printf("%d\t",tong1);
+		printf("\n+Tong so xe dung hen: ");
+		ghi<<"\n+Tong so xe dung hen: ";
+		printf("%d\t",tong1);
+		ghi<<tong1;
+		
+		printf("\n-Ten xe dung hen: \t");
+		ghi<<"\n-Ten xe dung hen:  ";
+		for(int i=0; i<n;i++){
+			if(ktratre[i]==0){
+				printf("%d\t",cv[i]);
+				ghi<<cv[i]<<" ";					
+			}
 
+		}
+	
+			//-----------------------------Phan chi xuat ra man hinh
+			printf("\n-Hieu so ngay xe oto:\t");			
+			for(int i=0; i<n;i++)
+			if(ktratre[i]==0)
+			printf("%d\t",hieusongay[i]);					
+			printf("\n-So ngay con du\nsau moi lan sua xe oto: ");			
+			for(int i=0; i<n;i++)
+			if(ktratre[i]==0)
+			printf("%d\t",songaythua[i]);
+			//------------------------------------------------------
+			
+		printf("\n-Tong so ngay thuc hien: ");
+		ghi<<"\n-Tong so ngay thuc hien: ";	
+		printf("%d\t",tong2);
+		ghi<<tong2;
+			
+		printf("\n+Tong so xe tre hen: ");
+		ghi<<"\n+Tong so xe tre hen: ";
+		printf("%d\t",demxe);
+		ghi<<demxe;
+			
+		if(demxe!=0){
+		printf("\n-Ten xe tre hen: ");
+		ghi<<"\n-Ten xe tre hen: ";
+		for(int i=0; i<n; i++){
+		 if(ktratre[i]==1){
+		 printf(" %d \t",x[i]);
+		 ghi<<x[i]<<"  ";		
+		  }
+		 }
+		}else {
+			printf("\nKhong co xe tre hen!");
+			ghi<<"\nKhong co xe tre hen!";
+		}
+			
+		printf("\n\nThu tu cua xe oto: \n");
+		  printf("\nXe oto:\t\t\t ");
+		  ghi<<"\nXe oto:\t\t\t ";
+		for(int i=0; i<n; i++){
+			printf("%d\t",x[i]);
+			ghi<<x[i]<<"  ";
+		}
+		  printf("\nThoi gian sua xe oto:\t ");
+		  ghi<<"\nThoi gian sua xe oto:\t ";
+		for(int i=0; i<n; i++){
+			printf("%d\t",y[i]);
+			ghi<<y[i]<<"  ";
+		}
+		  printf("\nThoi diem giao xe oto:   ");
+		  ghi<<"\nThoi diem giao xe oto:   ";
+		for(int i=0; i<n; i++){
+			printf("%d\t",z[i]);
+			ghi<<z[i]<<"  ";
+		}
+					
 	 printf("\n\nXuat kq thanh cong!\n\n");
+	 
 }
-void Swap(int &a, int &b)
+
+void Swap(int &a, int &b) 
 {
 	 int temp;
         temp = a;
@@ -148,7 +204,7 @@ void Swap(int &a, int &b)
    
 }
 
-void Xeptheoz(){
+void Xeptheoz(){ // xep thu tu cua xe oto theo thoi diem giao xe
 	for(int i=0; i< n-1; i++)
 		for(int j=i+1; j < n; j++){
 		 if(z[i]>z[j]){
@@ -160,50 +216,54 @@ void Xeptheoz(){
 }
 
 void Sapxep(){
-		int tamx, tamy, tamz;
+		int tamx, tamy, tamz, tamkt;
 		tong1=0; // tong so xe giao dung hen
 		tong2=0;// tong so ngay thuc hien
 	
 	 for(int i=0; i< n; i++){
 			
-		hieusongay[i]= z[i] - y[i];
-					
-			if( (tong2 + y[i]) <= z[i] ){
-				
+		
+					// neu tong so ngay t_hien truoc do + so ngay t_hien cua xe thu i 
+			if( (tong2 + y[i]) <= z[i] ){ //< = thoi diem giao xe cua xe thu i, thi:
 				tong2+=y[i]; 
 				tong1+=1;
-				songaythua[i]= z[i] - tong2;
-				
 				printf("\nNhap cv thanh cong!\n");
-				
+				ktratre[i] = 0;
 				cv[i]=x[i];	
-				printf("\n%d",cv[i]);
-				
+				printf("\n %d",cv[i]);
+				songaythua[i]= z[i] - tong2;
+				hieusongay[i]= z[i] - y[i]; 
 			}
-			else{
-				if (i== n-1) {  // TH xe o cuoi khong thoa man dkien -> xoa nhung xe khong dung han
-					n--;
-					return;
-				}
-				tamx = x[i]; // chuyen xe ko thoa dkien xuong cuoi
+			else if( ktratre[i] != 1){
+			//khong thoa man dkien
+				ktratre[i] = 1;
+				demxe+=1;	
+				tamx = x[i];//gan sang bien tam
 				tamy = y[i];
 				tamz = z[i];
-				for(int j = i; j < n; j++) //xet vi tri i cua xe
-				if(j== (n-1)){ // neu no dang vi tri cuoi thi = chinh no
+				tamkt = ktratre[i];
+				for(int j = i; j < n; j++){ //xet tai vi tri hien tai cua xe
+				if(j== (n-1)){  // neu no dang vi tri cuoi thi = chinh no
 					x[j] = tamx;
 					y[j] = tamy;
 					z[j] = tamz;
-				}
-				else{				// nguoc lai, chuyen xe ra sau 1 vi tri j+1
+					ktratre[j] = tamkt;
+						
+					//	demxe+=1; // dem tong xe tre hen
+						//	ktratre[t]=1;
+				} else{				// nguoc lai, chuyen xe ra sau 1 j, vi tri j+1
 				
-				x[j] = x[j+1];
-				y[j] = y[j+1];
-				z[j] = z[j+1];
-			}
-			i--;
+					x[j] = x[j+1];
+					y[j] = y[j+1];
+					z[j] = z[j+1];
+					ktratre[j] = ktratre[j+1];
+				
+				 }			 
+		}
+			i--;			
 			}
 
-}
+		}
 }
 
 int main(){
@@ -220,12 +280,11 @@ int main(){
 	printf("\nBat dau xep: \n"); 
 	Xeptheoz();
 	printf("\nKQ sau khi sx: \n");
-	Xuat();
-	
-	printf("\nBat dau tinh: \n");
+	Xuat();	
+	printf("\n\nBat dau tinh: \n");
 	Sapxep();
-	Xuatkq();
-	printf("\nKQ: \n");
+	printf("\nKQ va ghi vao file: \n");
+	Xuatfilekq();
 	Xuat();
 	getch();
 }
